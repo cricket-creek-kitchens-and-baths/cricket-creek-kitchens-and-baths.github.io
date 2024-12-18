ae9bfd7b6094c7bc6315842f00b868f56ca1abd6
Kelly Ripple <kelly.m.ripple@gmail.com>
2024-12-17 20:24:14 -0500

    play with hover styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -31,24 +31,18 @@ export function Header() {
       </div>
       <div className="header-nav">
         <div className="header-nav-content">
-          <NavLink
-            aria-label={title}
-            className="logo-link"
-            to={Route.Home}
-          >
+          <NavLink aria-label={title} className="logo-link" to={Route.Home}>
+            <svg className="svg-logo" viewBox="0 0 17.974 18.372">
+              <path
+                d="M54.808 48.465c-4.771-.885-.747-1.58-3.699-2.245-1.133-.098-1.538-.468-1.596-.859-.07-.479.108-.76-1.42-.705-.679.025-1.143-.186-1.225-.448-.096-.307.058-.789-.945-.516v-.608c1.801-.25 1.406.26 1.593.492.187.233 1.084-.43 2.829.163.778.264-.663.8 2.077.814 1.686.009 2.496.579 2.45.807-.223 1.124.425.933.856.924 1.883-.04 2.59.826 2.31 1.222-.42.592.06.759.35.801 5.881.857 1.493 3.343-1.512 2.663-.646-.222-2.955-.315-2.068-2.505zm-8.87-6.446-.015-8.948h17.973l-.007 17.26c-2.464-6.837-12.734-8.086-17.95-8.312Zm10.651 1.182v-1.228H55.4V43.2zm2.123-1.048-2.706-3.291h-4.758V37.84h-1.156v1.025l-.5-.006-2.67 2.762c2.716.295 5.132.41 7.574.54l1.483-2.163 2.17 2.655z"
+                transform="translate(-45.923 -33.07)"
+              />
+            </svg>
             <Image
               alt={title}
-              className="logo"
+              className="logo-title"
               draggable={false}
-              // height="122"
-              src="/images/logo.png"
-              // width="357"
-            />
-            <Image
-              aria-hidden={true}
-              className="logo-overlay"
-              draggable={false}
-              src="/images/logo-overlay.png"
+              src="/images/logo-title.png"
             />
           </NavLink>
 
