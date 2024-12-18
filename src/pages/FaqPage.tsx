6c1cc6bf83f5f7d4a304ef97e65bd9ec9d997991
Kelly Ripple <kelly.m.ripple@gmail.com>
2024-12-18 14:06:22 -0500

    add faq page

--- src/pages/FaqPage.tsx
+++ src/pages/FaqPage.tsx
@@ -1,7 +1,17 @@
+import { faqPageContent } from '@/content';
+
 export function FaqPage() {
   return (
     <article className="page" data-name={FaqPage.name}>
-      FaqPage
+      <section>
+
+      {faqPageContent.map(({ question, answer }, index) => (
+        <p key={index}>
+          <span className="content-header">{question}</span>
+          {answer}
+        </p>
+      ))}
+      </section>
     </article>
   );
 }
